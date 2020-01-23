ee7bc1e34a38a71684dfa810fcaa4840a825a071
Amarnath <vnnavul@amazon.com>
2020-01-23 15:16:59 +0530

    Components App: Using Props

--- components/src/index.js
+++ components/src/index.js
@@ -5,7 +5,9 @@ import CommentDetail from "./CommentDetail";
 const App = () =>{
     return (
         <div className= "ui container comments">
-            <CommentDetail />
+            <CommentDetail author="Amar" />
+            <CommentDetail author="Akbar" />
+            <CommentDetail author="Antony" />
         </div>
     );
     

51e9d376c197736fa2b9305aac046172f8ebc979
Amarnath <vnnavul@amazon.com>
2020-01-23 15:21:42 +0530

    Components App: Multiple Props

--- components/src/index.js
+++ components/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from "./CommentDetail";
+import faker from "faker";
 
 const App = () =>{
     return (
         <div className= "ui container comments">
-            <CommentDetail author="Amar" />
-            <CommentDetail author="Akbar" />
-            <CommentDetail author="Antony" />
+            <CommentDetail author="Amar" timeAgo="Today at 6:00 PM" commentText="Nice blog post yar!!!" avatar={faker.image.avatar()}/>
+            <CommentDetail author="Akbar" timeAgo="Today at 6:01 PM" commentText="Amar, I +1 you!!" avatar={faker.image.avatar()}/>
+            <CommentDetail author="Antony" timeAgo="Today at 6:10 PM" commentText="The best blog post yar!!!" avatar={faker.image.avatar()}/>
         </div>
     );
     

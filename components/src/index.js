40d2d70b45d8a6ede2b466db2f099878ed98ddb4
AmarnathN <vayuteja@gmail.com>
2021-01-15 18:46:59 +0530

    fix issues with faker images

--- components/src/index.js
+++ components/src/index.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
-import faker from "faker";
+// import faker from "faker";
 
-const App = () =>{
-    return (
-        <div className= "ui container comments">
-            <CommentDetail author="Amar" timeAgo="Today at 6:00 PM" commentText="Nice blog post yar!!!" avatar={faker.image.avatar()}/>
-            <CommentDetail author="Akbar" timeAgo="Today at 6:01 PM" commentText="Amar, I +1 you!!" avatar={faker.image.avatar()}/>
-            <CommentDetail author="Antony" timeAgo="Today at 6:10 PM" commentText="The best blog post yar!!!" avatar={faker.image.avatar()}/>
-        </div>
-    );
-    
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <CommentDetail
+        author="Amar"
+        timeAgo="Today at 6:00 PM"
+        commentText="Nice blog post yar!!!"
+        avatar=" https://picsum.photos/id/1035/200"
+      />
+      <CommentDetail author="Akbar" timeAgo="Today at 6:01 PM" commentText="Amar, I +1 you!!" avatar=" https://picsum.photos/id/1062/200" />
+      <CommentDetail
+        author="Antony"
+        timeAgo="Today at 6:10 PM"
+        commentText="The best blog post yar!!!"
+        avatar="https://picsum.photos/id/237/200"
+      />
+    </div>
+  );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
 
 // // If you want your app to work offline and load faster, you can change
 // // unregister() to register() below. Note this comes with some pitfalls.
